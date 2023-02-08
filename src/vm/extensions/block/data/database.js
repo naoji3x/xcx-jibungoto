36e8bfe523d70b09f205595eebf042ba1685f4de
naoji3x <naoji.3x@gmail.com>
2023-02-08 22:52:00 +0900

    自家用車の運転時のフットプリント計算のテストを追加

---
+++ src/vm/extensions/block/data/database.js
@@ -0,0 +1,16 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+import { footprints } from './footprints';
+import { parameters } from './parameters';
+export var getBaselineAmount = function (domain, item) { return (__assign({}, footprints['baseline_' + domain + '_' + item + '_amount'])); };
+export var getBaselineIntensity = function (domain, item) { return (__assign({}, footprints['baseline_' + domain + '_' + item + '_intensity'])); };
+export var getParameter = function (category, key) { return (__assign({}, parameters[category + '_' + key])); };
